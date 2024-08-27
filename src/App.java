21a2ee5d018a2b2b337e4625c90e656b92cc3b5d
Yazan <yazan.kirresh@hotmail.com>
2024-08-27 19:38:57 +0300

    Needs to fix when the number is valid and not a perfect number

--- src/App.java
+++ src/App.java
@@ -7,22 +7,26 @@ public class App {
             return -1;
         }else if ((int)Math.sqrt(n) * (int)Math.sqrt(n) == n){
             // the provided number is a perfect number
-            return 1;
+            return 2;
         }else {
             // the number is valid and not a perfect number
-            for (int i = 0; i < n; i++){
-               // if()
+            for (int i = 1; i < n; i++){
+               if((int)Math.sqrt(n-i) * (int)Math.sqrt(n-i) + i == n ){
+                System.out.println("Root: " + (int)Math.sqrt(n-i) + " Loop: " + i);
+                System.out.println((int)Math.sqrt(n-i));
+                System.out.println(i);
+                return 2+i;
+               }
             }
-
         }
         return n;
     }
 
     public static void main(String[] args) throws Exception {
-        int number = 12;
+        int number = 146;
         int output = numSquares(number);
         if (output == 1){
-            System.out.println("The provided number" + number +"is a perfect squre");
+            System.out.println("The provided number " + number +" is a perfect squre");
         }else if (number == output){
             System.err.println("Could not find a valid square other than the '1'");
         }else if (output == -1){
