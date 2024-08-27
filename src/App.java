38d58a3572ea42882e8e9413ca9a7223cf601fe9
Yazan <yazan.kirresh@hotmail.com>
2024-08-27 19:06:23 +0300

    Added a case where the number is a perfect root itself

--- src/App.java
+++ src/App.java
@@ -4,13 +4,21 @@ public class App {
         if (n == 0 || n < 0){
             System.out.println("The entered number is not valid");
             return -1;
+        }else if ((int)Math.sqrt(n) * (int)Math.sqrt(n) == n){
+            return 1;
         }
         return -1;
     }
 
     public static void main(String[] args) throws Exception {
-        int number = 0;
+        int number = 12;
         int output = numSquares(number);
-        System.out.println("The least number of perfect squares is: " + output);
+        if (output == 1 ){
+            System.out.println("The provided number" + number +"is a perfect squre");
+        }else if (output != -1){
+            System.out.println("The least number of perfect squares is: " + output);
+        }else {
+            System.out.println("An error occured, please try again later ;(");
+        }
     }
 }
