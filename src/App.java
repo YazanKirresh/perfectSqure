4ff5b7a9776cfb22ff20f9841839088c0ab9b4bb
Yazan <yazan.kirresh@hotmail.com>
2024-08-27 19:14:14 +0300

    Added the 1 to be always the squre just incase we could not find a valid squre

--- src/App.java
+++ src/App.java
@@ -3,22 +3,32 @@ public class App {
     public static int numSquares(int n) {
         if (n == 0 || n < 0){
             System.out.println("The entered number is not valid");
+            // the provided number does not have a valid root
             return -1;
         }else if ((int)Math.sqrt(n) * (int)Math.sqrt(n) == n){
+            // the provided number is a perfect number
             return 1;
+        }else {
+            // the number is valid and not a perfect number
+            for (int i = 0; i < n; i++){
+               // if()
+            }
+
         }
-        return -1;
+        return n;
     }
 
     public static void main(String[] args) throws Exception {
         int number = 12;
         int output = numSquares(number);
-        if (output == 1 ){
+        if (output == 1){
             System.out.println("The provided number" + number +"is a perfect squre");
-        }else if (output != -1){
-            System.out.println("The least number of perfect squares is: " + output);
-        }else {
+        }else if (number == output){
+            System.err.println("Could not find a valid square other than the '1'");
+        }else if (output == -1){
             System.out.println("An error occured, please try again later ;(");
+        }else {
+            System.out.println("The least number of perfect squares is: " + output);
         }
     }
 }
